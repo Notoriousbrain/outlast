70f6fc656f5e853ba889a48cdf886f9f56e26421
himanshu <hmxnsu.blvck@gmail.com>
2023-07-13 10:46:24 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+
+  return (
+    <div className="App text-[#ece6d9] bg-[#1a1a1e] font-bold text-6xl flex justify-center items-center min-h-screen "
+    >
+      OUTLAST
+    </div>
+  )
+}
+
+export default App
