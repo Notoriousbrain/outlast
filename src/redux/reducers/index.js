2053ca70e7c369c7fb0567e4f4bb7dbcb3b1a558
Aadarsh Jha <aadarshjha1401@gmail.com>
2023-07-13 17:08:05 +0530

    redux setup

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux"
+import { userData } from "./userReducers"
+
+export default combineReducers({ userData })
---
+++ src/redux/reducers/userReducers.js
@@ -0,0 +1,9 @@
+import * as actionType from '../actions/actionTypes'
+
+const initialState = {
+  profile: []
+}
+
+export const userData = (state = initialState, action) => {
+  return state
+}
