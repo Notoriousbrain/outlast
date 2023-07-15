b11529b423c2685e251afe95d8b09fe0a6718d81
himanshu <hmxnsu.blvck@gmail.com>
2023-07-16 03:07:19 +0530

    'merged'

--- src/components/TripView/TripRouteCard.jsx
+++ src/components/TripView/TripRouteCard.jsx
@@ -5,7 +5,7 @@ import { useJsApiLoader, GoogleMap, Autocomplete } from '@react-google-maps/api'
 
 const TripRouteCard = ({ editable }) => {
 
-  const libraries = ["places"]
+  const [ libraries ] = useState(['places']);
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
@@ -14,34 +14,46 @@ const TripRouteCard = ({ editable }) => {
 
   return (
     <div
-      className=" border border-border p-4 rounded-[30px]  "
+      className=" border border-border rounded-[30px] overflow-hidden "
     >
       <div
-        className="flex flex-col gap-2 justify-center items-center rounded-[20px] overflow-hidden h-[400px] w-[500px] "
+        className="flex flex-col gap-2 justify-center items-center overflow-hidden h-[400px] w-[500px] "
       >
         {
           isLoaded && ( <>
           <GoogleMap
-          mapContainerStyle={{height: "105%", width: "102%"}}
+          mapContainerStyle={{height: "101%", width: "100%"}}
           zoom={6}
           center={{lat: 22, lng: 72}}
           options={{
             zoomControl: true,
+            
             mapTypeControl: false,
             streetViewControl: false,
             fullscreenControl: false,
+            center: {lat: 22, lng: 72},
+            zoom: 6,
+            restriction: {
+              latLngBounds: {
+                north: 37.084107,
+                south: 7.635,
+                west: 68.7,
+                east: 97.4,
+              },
+              strictBounds: true,
+            },
           }}
+
           // onLoad={(map) => {
           //   console.log(map)
           // }}
         >
         </GoogleMap>
           <Autocomplete
-            onLoad={(autocomplete) => {
-              console.log(autocomplete)
+            onLoad={(Autocomplete) => {
+              console.log(Autocomplete)
             }}
             options={{
-              // types: ["(cities)", "(regions)"],
               componentRestrictions: {country: "in"}
 
             }}
@@ -49,7 +61,7 @@ const TripRouteCard = ({ editable }) => {
             <input
               type="text"
               placeholder="Search"
-              // onChange={(e) => console.log(e.target.value)}
+              onChange={(e) => console.log(e.target.value)}
               // onKeyUp={(e) => console.log(e.target.value)}
               // onClick={(e) => console.log(e.target.value)}
               className=" border border-border rounded-[20px] p-2 w-full "
@@ -58,6 +70,11 @@ const TripRouteCard = ({ editable }) => {
           </>
           )
         }
+      </div>
+      <div
+        className="flex flex-col gap-2 justify-center items-center "
+      >
+
       </div>
     </div>
   )
